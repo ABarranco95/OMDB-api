f9c75d95d992a040de3c43263b9874f4936404f3
Angel Barranco <angel@Angels-Mac-mini.local>
2021-09-27 16:45:53 -0700

    add useEffect and useState hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react'
+
+
+import MovieCard from './components/MovieCard'
+
+
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+
+  const getMoviesBySearchTerm = async (search) => {
+    const response = await fetch(`http://www.omdbapi.com/?s=${search}&apikey=${process.env.REACT_APP_API_KEY}`)
+    const data = await response.json()
+    console.log(data);
+}
+
+  useEffect(() => {
+    getMoviesBySearchTerm('Lord of the Rings').then(movieList => {
+      setMovies(movieList);
+    })
+  }, [])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>OMDb Movies</h1>
+
+      <div className='movie-list'>
+        
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieCard = () => {
+    return (
+        <div className='movie'>
+            This is a movie component
+        </div>
+    )
+}
+
+export default MovieCard
+
